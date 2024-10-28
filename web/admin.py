9431f7dcffc3d112f6d87eb2c654b03c28977caf
Abbath <alucard.erick.mega@gmail.com>
2024-10-28 18:02:08 -0300

    Avances

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Flan, ContactForm
 
 # Register your models here.
+@admin.register(Flan)
+class FlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'flan_uuid', 'is_private')
+    
+admin.site.register(ContactForm)
--- web/views.py
+++ web/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Flan
+from .form import ContactFormForm
+
+
 
 def index(request):
-    return render(request, 'web/index.html', {'content': 'índice'})
+    flanes = Flan.objects.filter(is_private=False)
+    return render(request, 'web/index.html', {'flanes': flanes})
 
 def about(request):
     return render(request, 'web/about.html', {'content': 'acerca'})
 
 def welcome(request):
-    return render(request, 'web/welcome.html', {'content': 'bienvenido cliente'})
+    flanes_privados = Flan.objects.filter(is_private=True)
+    return render(request, 'web/welcome.html', {'flanes': flanes_privados})
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactFormForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('exito')
+    else:
+        form = ContactFormForm()
+    return render(request, 'web/contact.html', {'form': form})
